60bc860f64a551762ae1e1db58c0ca099d1e4647
Rapi <joaooviitorr@hotmail.com>
2023-06-21 14:41:18 -0300

    fix: remove config file

--- jest-e2e.config.ts
+++ jest-e2e.config.ts
@@ -4,7 +4,6 @@ import jestConfig from './jest.config';
 
 const config: Config = {
   ...jestConfig,
-  testEnvironment: './test/configs/e2e-config.ts',
   testRegex: '.e2e.spec.ts$',
   coverageDirectory: './coverage/e2e',
 };
