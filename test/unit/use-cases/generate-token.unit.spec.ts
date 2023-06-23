4f0cc9bc3345e93934fffd4c7c4f3f425ed7262e
Rapi <joaooviitorr@hotmail.com>
2023-06-23 09:08:32 -0300

    feat: added console.log

--- test/unit/use-cases/generate-token.unit.spec.ts
+++ test/unit/use-cases/generate-token.unit.spec.ts
@@ -7,6 +7,10 @@ describe('Generate token use case', () => {
   let generateToken: GenerateToken;
 
   beforeEach(() => {
+    console.log({
+      privateKey: process.env.JWT_PRIVATE_KEY,
+      publicKey: process.env.JWT_PUBLIC_KEY,
+    });
     jwtService = new JwtService({
       privateKey: process.env.JWT_PRIVATE_KEY,
       publicKey: process.env.JWT_PUBLIC_KEY,
