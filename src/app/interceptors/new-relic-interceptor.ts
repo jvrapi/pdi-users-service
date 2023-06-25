13a48288e4287c24befba34b913b4a43142a729b
João  Lima <joao.lima@somoseducacao.com.br>
2023-06-25 11:07:25 -0300

    fix: jwt expiration env

--- src/app/interceptors/new-relic-interceptor.ts
+++ src/app/interceptors/new-relic-interceptor.ts
@@ -11,6 +11,7 @@ const newrelic = require('newrelic');
 @Injectable()
 export class NewrelicInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    console.log(context.getHandler().name);
     return newrelic.startWebTransaction(context.getHandler().name, function () {
       const transaction = newrelic.getTransaction();
       return next.handle().pipe(tap(() => transaction.end()));
